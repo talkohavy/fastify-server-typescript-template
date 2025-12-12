4ac2f1ab552ee638e63cc06df4e146a117bc1e6c
talkohavy <51020442+talkohavy@users.noreply.github.com>
2025-12-12 16:04:39 +0200

    bugfix: validation examples module never passed app

--- src/modules/validation-examples/validation-examples.module.ts
+++ src/modules/validation-examples/validation-examples.module.ts
@@ -2,11 +2,13 @@ import type { FastifyInstance } from 'fastify';
 import { ValidationExamplesController } from './controllers/validation-examples.controller';
 
 export class ValidationExamplesModule {
-  constructor() {
+  constructor(private readonly app: FastifyInstance) {
     this.initializeModule();
   }
 
-  private async initializeModule(): Promise<void> {}
+  private async initializeModule(): Promise<void> {
+    this.registerController(this.app);
+  }
 
   registerController(app: FastifyInstance): void {
     const validationExamplesController = new ValidationExamplesController(app);
