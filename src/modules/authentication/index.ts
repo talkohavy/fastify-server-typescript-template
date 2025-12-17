7e5e3fcd5b2113f5a4a396c9d16bc58cf1fe3e8c
talkohavy <51020442+talkohavy@users.noreply.github.com>
2025-12-17 16:41:59 +0200

    temporarily remove Authentication module

--- src/modules/authentication/index.ts
+++
@@ -1,1 +0,0 @@
-export { AuthenticationModule } from './authentication.module';
--- src/modules/authentication/services/interfaces/token-generation.interface.ts
+++
@@ -1,7 +0,0 @@
-export type CreateAccessTokenProps = {
-  id: string;
-};
-
-export type CreateRefreshTokenProps = {
-  id: string;
-};
