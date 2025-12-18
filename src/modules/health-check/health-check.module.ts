606443d1e11d42102fd415c1ba80842e0a49216c
talkohavy <51020442+talkohavy@users.noreply.github.com>
2025-12-18 11:20:58 +0200

    HealthCheckModule is conditioned by IS_STANDALONE_MICRO_SERVICES

--- src/modules/health-check/health-check.module.ts
+++ src/modules/health-check/health-check.module.ts
@@ -1,15 +1,20 @@
+import type { FastifyInstance } from 'fastify';
+import { IS_STANDALONE_MICRO_SERVICES } from '../../common/constants';
 import { HealthCheckController } from './health-check.controller';
 
 export class HealthCheckModule {
-  constructor(private readonly app: any) {
+  constructor(private readonly app: FastifyInstance) {
     this.initializeModule();
   }
 
   private initializeModule(): void {
-    this.attachControllers(this.app);
+    // Only attach routes if running as a standalone micro-service
+    if (IS_STANDALONE_MICRO_SERVICES) {
+      this.attachControllers(this.app);
+    }
   }
 
-  private attachControllers(app: any): void {
+  private attachControllers(app: FastifyInstance): void {
     const controller = new HealthCheckController(app);
 
     controller.registerRoutes();
